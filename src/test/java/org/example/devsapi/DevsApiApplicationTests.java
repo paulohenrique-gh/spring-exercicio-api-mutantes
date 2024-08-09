4210a3222fd18eabae32c2a2c555e293391e7a7f
Paulo Henrique <ph.1928@hotmail.com>
2024-08-09 19:39:27 -0300

    Commit inicial
    configuração do application.properties

---
+++ src/test/java/org/example/devsapi/DevsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.devsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
