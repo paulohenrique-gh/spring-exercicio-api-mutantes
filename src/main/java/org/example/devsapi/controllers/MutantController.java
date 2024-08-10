10885dea24ad073f0de6916c0e5700ad1e40899b
Paulo Henrique <ph.1928@hotmail.com>
2024-08-09 22:01:42 -0300

    Adiciona controller Mutant

---
+++ src/main/java/org/example/devsapi/controllers/MutantController.java
@@ -0,0 +1,57 @@
+package org.example.devsapi.controllers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.devsapi.dtos.MutantDto;
+import org.example.devsapi.models.Mutant;
+import org.example.devsapi.services.MutantService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("mutants")
+@RequiredArgsConstructor
+public class MutantController {
+
+    private final MutantService mutantService;
+
+    @PostMapping
+    public ResponseEntity<Object> addMutant(@RequestBody @Valid MutantDto mutantDto) {
+        if (!mutantService.authenticate(mutantDto.password())) {
+            return getUnauthorizedResponse();
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(mutantService.addMutant(mutantDto));
+    }
+
+    @GetMapping("{id}/enemies-defeated-summary")
+    public ResponseEntity<Map<String, Object>> getEnemiesDefeatedSummary(@PathVariable UUID id) {
+        Mutant mutant = mutantService.findById(id);
+        Map<String, Object> enemiesDefeatedSummary = new HashMap<>();
+        enemiesDefeatedSummary.put("aliensDefeated", mutant.getAliensDefeated());
+        enemiesDefeatedSummary.put("demonsDefeated", mutant.getDemonsDefeated());
+        if (mutant.getIsEligibleForEspada()) {
+            enemiesDefeatedSummary.put("specialMessage", "You have been summoned to join E.S.P.A.D.A");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(enemiesDefeatedSummary);
+    }
+
+    @GetMapping("{id}/espada-eligibility")
+    public ResponseEntity<Object> checkEspadaEligibility(@PathVariable UUID id) {
+        Mutant mutant = mutantService.findById(id);
+        Map<String, Boolean> responseMsg = new HashMap<>();
+        responseMsg.put("eligibleForEspada", mutant.getIsEligibleForEspada());
+        return ResponseEntity.status(HttpStatus.OK).body(responseMsg);
+    }
+
+    private ResponseEntity<Object> getUnauthorizedResponse() {
+        Map<String, String> responseMsg = new HashMap<>();
+        responseMsg.put("error", "Invalid password");
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseMsg);
+    }
+}
